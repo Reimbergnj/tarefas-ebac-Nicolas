6a5b4a0cc1f688f3fc53c6282ff0362ecdc70478
Reimbergnj <reimbergnicolas01@gmail.com>
2025-10-24 08:57:17 -0300

    Adicionando exercício de boxing

---
+++ src/br/com/njaco/Tarefa_Wrapper/TarefaMod9.java
@@ -0,0 +1,8 @@
+package br.com.njaco.Tarefa_Wrapper;
+
+public class TarefaMod9 {
+    public static void main (String[] args){
+        Integer i = 1;
+        System.out.println("A conversão do tipo primitivo Integer para a wrapper i resulta em: Integer i = " + i);
+    }
+}
